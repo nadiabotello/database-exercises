512c6fab5b9d419ce764ef35c2023c1cbee0bcdf
Nadia Botello <botello.nadia.g@gmail.com>
2019-06-03 16:03:18 -0500

    completed basic statements exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,12 +2,14 @@ USE codeup_test_db;
 
 SELECT 'Albums released after 1991' AS 'Exercise 1';
 
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'Albums with the genre disco' AS 'Exercise 2';
 
-SELECT * FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE genre = 'Disco';
 
 SELECT 'Albums by The Beatles (never will I delete Whitney)' AS 'Exercise 3';
 
-SELECT * FROM albums WHERE artist = 'The Beatles';
+DELETE FROM albums WHERE artist = 'The Beatles';
+
+SELECT * FROM albums;
