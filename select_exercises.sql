80cb070acaf45dc89f8dbbba6b42d2121765bc19
Nadia Botello <botello.nadia.g@gmail.com>
2019-06-04 10:42:17 -0500

    walkthrough variations

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,8 +1,14 @@
 USE codeup_test_db;
 
-SELECT 'The name of all albums by Pink Floyd' AS 'Exercise 1';
+-- SELECT 'The name of all albums by Pink Floyd' AS 'Exercise 1';
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+-- SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+-- Returning info header in same table (both select phrases in one)
+
+SELECT name AS 'The name of all albums by Pink Floyd', artist AS 'Band'
+    FROM albums
+    WHERE artist = 'Pink Floyd';
 
 SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Exercise 2';
 
@@ -16,6 +22,9 @@ SELECT 'Which albums were released in the 1990s' AS 'Exercise 4';
 
 SELECT * FROM albums WHERE release_date >= 1990 and release_date <= 1999;
 
+-- WHERE release_date LIKE '199%';
+-- WHERE release_date BETWEEN 1990 AND 1999;
+
 SELECT 'Which albums had less than 20 million certified sales' AS 'Exercise 5';
 
 SELECT * FROM albums WHERE sales < 20;
@@ -23,3 +32,4 @@ SELECT * FROM albums WHERE sales < 20;
 SELECT 'All the albums with a genre of Rock' AS 'Exercise 6';
 
 SELECT * FROM albums WHERE genre = 'Rock';
+
