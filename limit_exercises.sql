824af809ea5e3debd0fabb34d67f1ad21725b9ac
Nadia Botello <botello.nadia.g@gmail.com>
2019-06-05 09:38:19 -0500

    completed limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,21 @@
+use employees;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+
+select *
+from employees
+where birth_date like '%12-25'
+  AND hire_date like '199%'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+select *
+from employees
+where birth_date like '%12-25'
+  AND hire_date like '199%'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
