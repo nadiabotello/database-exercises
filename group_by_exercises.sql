4f3c116863f468062bfdaefa6ef270256c866b72
Nadia Botello <botello.nadia.g@gmail.com>
2019-06-05 14:43:37 -0500

    updated with clarifications

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,22 +4,26 @@ select * from titles;
 
 SELECT DISTINCT title FROM titles;
 
+SELECT last_name
+from employees
+where last_name like 'e%e'
+GROUP BY last_name;
+
 SELECT first_name, last_name
 from employees
 where last_name like 'e%e'
 GROUP BY first_name, last_name;
 
-select last_name
+select distinct last_name
+from employees
+where last_name like '%q%' and last_name not like '%qu%';
+
+select COUNT(last_name), last_name
 from employees
 where last_name like '%q%' and last_name not like '%qu%'
 GROUP BY last_name;
 
-select COUNT(last_name)
-from employees
-where last_name like '%q%' and last_name not like '%qu%'
-GROUP BY last_name;
-
-select gender, count(*)
+select gender, count(gender)
 from employees
 where first_name in ('Irena', 'Vidya','Maya')
 GROUP BY gender;
