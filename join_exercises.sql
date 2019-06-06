e0bf832e8d32f26ae21eed2756260bfd4bb647fe
Nadia Botello <botello.nadia.g@gmail.com>
2019-06-06 10:51:58 -0500

    first half of employees database joins exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,27 +1,26 @@
-use join_test_db;
+use employees;
 
-show tables;
+select * from employees;
+select * from departments;
+select * from dept_manager;
 
-INSERT INTO users (name, email, role_id) VALUES
-('jim', 'jim@example.com', 2),
-('anne', 'anne@example.com', null),
-('ethel', 'ethel@example.com', 2),
-('dave', 'dave@examples.com', 2);
+-- departments with department managers' names
 
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'Department Manager', departments.dept_name AS 'Department'
+FROM employees
+JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
+JOIN departments ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date = '9999-01-01';
 
-INSERT INTO roles (name) VALUES ('admin');
-INSERT INTO roles (name) VALUES ('author');
-INSERT INTO roles (name) VALUES ('reviewer');
-INSERT INTO roles (name) VALUES ('commenter');
+-- departments currently managed by women
 
-SELECT users.name AS user_name, roles.name AS role_name
-FROM users
-RIGHT JOIN roles ON users.role_id = roles.id;
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'Department Manager', departments.dept_name AS 'Department'
+FROM employees
+         JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
+         JOIN departments ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date = '9999-01-01' AND gender = 'F';
 
-SELECT roles.name, COUNT(*) FROM users
-JOIN roles ON users.role_id = roles.id
-GROUP BY roles.name
-ORDER BY COUNT(*) ASC, roles.name
-LIMIT 10;
+-- titles of employees currently working in customer service dept with count
+
 
 
